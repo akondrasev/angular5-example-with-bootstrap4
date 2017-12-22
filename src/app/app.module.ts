42a3ff43d201d7fb145342f7231f143c3dfdd01a
anton.kondrasev <anton.kondrasev@live.com>
2017-12-22 12:14:30 +0200

    bootstrap style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule
